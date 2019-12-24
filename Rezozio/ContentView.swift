e22c7ea35e0593c1c0e0424612fdf3ef6461acd4
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2019-12-24 17:50:51 +0100

    Initial Commit

---
+++ Rezozio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rezozio
+//
+//  Created by eldin smakic on 24/12/2019.
+//  Copyright © 2019 eldin smakic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
