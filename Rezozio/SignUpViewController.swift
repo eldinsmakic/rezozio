5c70a79632ced7194d9873bcfea4b6cda1e3276a
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2019-12-24 22:42:53 +0100

    [Feat] : add new viewControler for signUp page

---
+++ Rezozio/SignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpViewController.swift
+//  Rezozio
+//
+//  Created by eldin smakic on 24/12/2019.
+//  Copyright © 2019 eldin smakic. All rights reserved.
+//
+
+import UIKit
+
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        FirebaseApp.configure()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
