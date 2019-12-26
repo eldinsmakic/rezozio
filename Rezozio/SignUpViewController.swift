ce783b2f3d8725cbf75c3e3bbe72c5d6a687f9f4
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2019-12-26 14:52:51 +0100

    [Feat] (SignUp) : add  - basic action on signup, login - check function for input

--- Rezozio/SignUpViewController.swift
+++ Rezozio/SignUpViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
@@ -16,6 +16,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,110 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    /*
+        OnClick on SignUp Button, check if
+        email and password are valid and try to create new user with firebase
+     */
+    @IBAction func OnSignOn(_ sender: UIButton)
+    {
+        var pass : Bool = true
+        if (isPasswordValid() == false)
+        {
+           
+                print("Bad password !")
+                pass = false
+           
+        }
+         if ( isEmailValid()  == false)
+        {
+                print("Bad Email")
+                pass = false
+        }
+        if (pass)
+        {
+            let pw = passwordTextField.text!
+            let email = emailTextField.text!
+            Auth.auth().createUser(withEmail: email, password: pw, completion:
+                           {
+                               (authResult,error) in
+                               if (error != nil)
+                               {
+                                   print(error.debugDescription)
+                               }
+                               else
+                               {
+                                   print("succes")
+                               }
 
+            })
+        }
+         
+    }
+    
+    
+    @IBAction func OnClickLogin(_ sender: UIButton) {
+                var pass : Bool = true
+               if (isPasswordValid() == false)
+               {
+                  
+                       print("Bad password !")
+                       pass = false
+                  
+               }
+                if ( isEmailValid()  == false)
+               {
+                       print("Bad Email")
+                       pass = false
+               }
+                if (pass)
+                {
+                    let pw = passwordTextField.text!
+                let email = emailTextField.text!
+                                   Auth.auth().signIn(withEmail: email, password: pw, completion:
+                                       {
+                                           (auth,error) in
+                                           if (error != nil)
+                                           {
+                                               print( error.debugDescription)
+                                           }
+                                           else
+                                           {
+                                               print("login success")
+                                           }
+                                       })
+            }
+               
+    }
+    
+    
+    
+    
+    /**
+                    Test if password has a length of 6
+     */
+    private func isPasswordValid() -> Bool
+    {
+        return stringMatchesPattern(string: passwordTextField.text!, pattern: "^.{6}")
+    }
+    
+    /**
+                        Test if a string matche a pattern
+     */
+    private func stringMatchesPattern(string: String, pattern : String) -> Bool
+    {
+        let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        return regex?.firstMatch(in: string , options: [], range: NSRange(location: 0, length: string.count)) != nil
+    }
+    
+    
+    /**
+                Test if email have a good email pattern
+     */
+    private func isEmailValid() -> Bool
+    {
+        return stringMatchesPattern(string: emailTextField.text!, pattern: "^[a-z0-9]+@[a-z0-9]+[.][a-z]+$")
+    }
     
     private func addRoundedCornerToTextView()
     {
@@ -32,6 +136,7 @@ class SignUpViewController: UIViewController {
         passwordTextField.layer.cornerRadius = 20
         emailTextField.layer.cornerRadius = 20
     }
+    
     
     private func addRoundedCornerToButton()
     {
