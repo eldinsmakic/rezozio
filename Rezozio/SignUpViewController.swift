e46e6aa554c337ce4ca1bf55f385e33ac0db2937
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2019-12-26 10:29:54 +0100

    [Feat] (SignUp) : add round to UI Components

--- Rezozio/SignUpViewController.swift
+++ Rezozio/SignUpViewController.swift
@@ -11,20 +11,32 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRoundedCornerToButton()
+        addRoundedCornerToTextView()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func addRoundedCornerToTextView()
+    {
+        usernameTextField.layer.cornerRadius = 20
+        passwordTextField.layer.cornerRadius = 20
+        emailTextField.layer.cornerRadius = 20
     }
-    */
+    
+    private func addRoundedCornerToButton()
+    {
+        signUpButton.layer.cornerRadius = 20
+        loginButton.layer.cornerRadius = 20
+    }
 
 }
