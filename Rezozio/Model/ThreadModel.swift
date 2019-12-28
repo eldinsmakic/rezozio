5b22fccb55bac3a2a38c7446aa9c8d766e8aa32e
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2019-12-28 17:45:24 +0100

    [Feat] : add Basic model of a thread

---
+++ Rezozio/Model/ThreadModel.swift
@@ -0,0 +1,25 @@
+//
+//  ThreadModel.swift
+//  Rezozio
+//
+//  Created by eldin smakic on 28/12/2019.
+//  Copyright © 2019 eldin smakic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThreadModel
+{
+    
+    var username: String?
+    
+    var image: UIImage?
+    
+    init(username : String , img : UIImage ) {
+        self.username = username
+        self.image = img
+        
+    }
+
+}
